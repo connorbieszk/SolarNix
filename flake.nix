182febda0fb20f4a83f3999ef5a98c07d1d50518
Connor B. <bieszcon29@stu.cedarburg.k12.wi.us>
2026-08-31 20:42:40 -0600

    inital flake

---
+++ flake.nix
@@ -0,0 +1,95 @@
+{
+  description = "Personal Network (solar) NixOS Configurations";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
+
+    nixos-wsl = { url = "github:nix-community/NixOS-WSL/main"; };
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, ... }:
+    let
+      # Shared Modules
+      sharedModules = [
+
+      ];
+
+      # Type Specific Modules
+      desktopModules = [
+        
+      ];
+
+      serverModules = [
+        
+      ];
+
+      laptopModules = [
+        
+      ];
+
+      wslModules = [
+        nixos-wsl.nixosModules.default
+
+        {
+          wsl.enable = true;
+          wsl.defaultUser = "pblez";
+        }
+      ];
+
+
+      # Type Module Definitions
+      typeModules = {
+        desktop = desktopModules;
+
+        laptop = desktopModules ++ laptopModules;
+
+        server = serverModules;
+
+        wsl = serverModules ++ wslModules;
+      };
+
+
+      ## Host Helper
+      mkHost =
+        {
+          hostname,
+          system,
+          type,
+          modules ? [],
+        }:
+
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+
+          modules =
+            [
+              {
+                networking.hostName = hostname; # Set Hostname here
+              }
+            ]
+
+            ## Add type specific modules
+            ++ (
+              if builtins.hasAttr type typeModules
+              then typeModules.${type}
+              else throw "Unknown host type: ${type}"
+            )
+
+            ## Add the shared modules
+            ++ sharedModules
+
+            ## Add the host specific modules
+            ++ modules;
+        };
+    in
+    {
+      nixosConfigurations = {
+        solarsatellite = mkHost {
+          hostname = "solarsatellite";
+          system = "x86_64-linux";
+          type = "wsl";
+          modules = [];
+        };
+      };
+    };
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+Device List:
+
+solarsatalite - WSL2 Setup
+solarnebula - OPi6+ Router
+solarbang - Cisco Router (Need to make a Nix -> Cisco Flake)
+solareclpise - Macbook Pro 2015 with NixOS installed
+solarpulsar - Main Laptop (Thinkpad X1 Carbon Gen 7)
+solarstar - NAS/Virt Server
+solarcomet - Small Server ( Windows 7 Era Thinkcentre )
+solarmeteor - Small Server ( Windows 7 Era Thinkcentre )
